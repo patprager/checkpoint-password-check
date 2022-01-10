a3a58d92b6d62d23cb92e50310281126985706de
patricia prager <florence.voltaire@web.de>
2022-01-10 14:27:05 +0100

    Thought structure

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1. Passwörter sind identisch
+// passwordEqual = false than is lowerCaseLetter, uperCaseLetter,
+// leastOneNumber, leastTenCharacters = false
+
+// 2. Passwort enthält mindestens einen "lowerCaseLetter"
+
+// 3. Passwort enthält mindestens einen "uperCaseLetter"
+
+// 4. Passwort enthält mind. eine Zahl
+
+// 5. Passwort ist mind. 10 Zeichen lang
+
+// 6. toggleButton – showPassword anzeigen des passwordInput or
+// hidePassword ausblenden des passwordInput
