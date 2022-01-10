8767f2ff801deeece8fab409a22b353dbf3bfdbe
patricia prager <florence.voltaire@web.de>
2022-01-10 22:02:24 +0100

    feat document.query.Selector variablen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// 1. Passwörter sind identisch
-// passwordEqual = false than is lowerCaseLetter, uperCaseLetter,
-// leastOneNumber, leastTenCharacters = false
+/* 1. Passwörter sind identisch
+ if passwordEqual = false than is lowerCaseLetter, uperCaseLetter,
+ leastOneNumber, leastTenCharacters = false */
 
 // 2. Passwort enthält mindestens einen "lowerCaseLetter"
 
@@ -12,3 +12,9 @@
 
 // 6. toggleButton – showPassword anzeigen des passwordInput or
 // hidePassword ausblenden des passwordInput
+
+const firstPWInput = document.querySelector("#first-password-input");
+const secondPwInput = document.querySelector("#second-password-input");
+const toggleButton = document.querySelector("#show-hide-button");
+
+function toggleBtn() {}
