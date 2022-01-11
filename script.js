664bc325d491f1f1dc83ee3d2106c1cd93d54c60
patricia prager <florence.voltaire@web.de>
2022-01-11 06:46:35 +0100

    function ideas

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-/* 1. Passwörter sind identisch
- if passwordEqual = false than is lowerCaseLetter, uperCaseLetter,
- leastOneNumber, leastTenCharacters = false */
-
-// 2. Passwort enthält mindestens einen "lowerCaseLetter"
-
-// 3. Passwort enthält mindestens einen "uperCaseLetter"
-
-// 4. Passwort enthält mind. eine Zahl
-
-// 5. Passwort ist mind. 10 Zeichen lang
-
-// 6. toggleButton – showPassword anzeigen des passwordInput or
-// hidePassword ausblenden des passwordInput
-
+const passwordInput = document.querySelector("input");
 const firstPWInput = document.querySelector("#first-password-input");
 const secondPwInput = document.querySelector("#second-password-input");
-const toggleButton = document.querySelector("#show-hide-button");
 
-function toggleBtn() {}
+const toggleButton = document.querySelector("#how-hide-button");
+const hideBtn = document.querySelector(" #hide-button");
+
+/*
+0. cklickedButton – showPassword anzeigen des passwordInput or
+ hidePassword ausblenden des passwordInput */
+
+function clickedBtn() {
+  const showBtn = document.getElementById("#show-hide-button").innerHTML;
+  const pwFields = document.getElementById("input");
+
+  if (showBtn === "Show Password") {
+    pwFields.setAttribute("type", "text");
+    document.getElementById("#how-hide-button").innerHTML = "Hide Password";
+  } else {
+    pwFields.setAttribute("input", "text");
+    document.getElementById("show-hide-button");
+  }
+}
+
+window.addEventListener("load", function () {
+  const showPw = this.document.getElementById("show-hide-button");
+  showPw.addEventListener("click", clickedBtn);
+});
+
+/*
+1. Passwörter sind identisch
+ if passwordEqual = false than is lowerCaseLetter, uperCaseLetter,
+ leastOneNumber, leastTenCharacters = false 
+
+ document.querySelector("#equal-password").addEventListener("click", clickedBtn);
+
+2. Passwort enthält mindestens einen "lowerCaseLetter"
+
+3. Passwort enthält mindestens einen "uperCaseLetter"
+
+4. Passwort enthält mind. eine Zahl
+
+5. Passwort ist mind. 10 Zeichen lang
+
+function filterModus (){
+    const passwordEqual = document.querySelector("input").addEventListener("click", button)
+    if () {
+        passwordEqual = true;
+    } else {
+        
+    }
+}
+  */
